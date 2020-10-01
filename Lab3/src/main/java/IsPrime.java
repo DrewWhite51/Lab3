2ea70d7cd9f087f1874e9e0ad263f09e5d72e132
Drew <drew.m.white51@gmail.com>
2020-10-01 15:23:55 -0500

    Updates.

--- Lab3/src/main/java/IsPrime.java
+++ Lab3/src/main/java/IsPrime.java
@@ -17,43 +17,77 @@ public class IsPrime {
     
     
       public static void main(String args[]) {
+          Scanner keyboard = new Scanner(System.in);
           
-      Scanner keyboard = new Scanner(System.in);
-      
-      
-      // Get number input from user
-      System.out.println("Enter the number to find if it is prime:");
-      int n = keyboard.nextInt(); 
+          System.out.println("Enter the number to find if it is prime:");
+          int n = keyboard.nextInt();
+
 
       
       
-      int m = 0;      
-      int i = 0;
-      int counter = 0;
-      m = n / 2;
+      checkPrime(n);
       
-      
-      if (n == 0 || n == 1) {
-         System.out.println(n + " not a prime number");
-      } else {
-         
-          for (i = 2; i <= m; i++) {
-            
-              
-              if (n % i == 0) {
-               System.out.println(n + " not a prime number");
-               counter = 1;
-               break;
-           
-              }
-         }
-         
-          if (counter == 0) {
-            System.out.println(n + " is a prime number");
-         }
-      }
+     
    }
       
+      static void checkPrime(int n){
+          
+          
+            int i,m=0,flag=0;
+            m=n/2;
+            if(n==0||n==1){
+                System.out.println(n+" is not a prime number.");
+                           }
+            else{
+                for(i=2;i<=m;i++){
+                    if(n%i==0){
+                        System.out.println(n+" is not a prime number.");
+                        flag=1;
+                        break;
+                                }
+                            }
+        
+                    if(flag==0) { System.out.println(n+" is a prime number."); }
+                                }
+}
       
+      
+//      public static void primeCheck() {
+//                
+//          Scanner keyboard = new Scanner(System.in);
+//
+//          
+//          
+//         System.out.println("Enter the number to find if it is prime:");
+//            
+//         int n = keyboard.nextInt(); 
+//         int m = 0;      
+//         int i = 0;
+//         int counter = 0;      
+//          
+//         if (n == 0 || n == 1) {
+//         
+//         System.out.println(n + " not a prime number");} else {
+//         
+//          for (i = 2; i <= m; i++) {
+//            
+//              
+//              if (n % i == 0) {
+//               System.out.println(n + " not a prime number");
+//               counter = 1;
+//               break;
+//           
+//              }
+//         }
+//         
+//          if (counter == 0) {
+//            System.out.println(n + " is a prime number");
+//         }
+//      }
+//            
+//   }
     
 }
+      
+    
+
